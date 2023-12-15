ff7854554e50641469b330a1e7712900f69d6ddb
michael <m.deimel@miceportal.com>
2023-12-15 17:44:56 +0100

    fix migrations

--- db/migrate/20231214095405_create_episode_statistics.rb
+++ db/migrate/20231214095405_create_episode_statistics.rb
@@ -1,5 +1,5 @@
 class CreateEpisodeStatistics < ActiveRecord::Migration[7.0]
   def change
-    create_view :episode_statistics
+    create_view :episode_statistics unless view_exists?(:episode_statistics)
   end
 end
--- db/migrate/20231215111028_create_episode_current_statistics.rb
+++ db/migrate/20231215111028_create_episode_current_statistics.rb
@@ -1,5 +1,5 @@
 class CreateEpisodeCurrentStatistics < ActiveRecord::Migration[7.1]
   def change
-    create_view :episode_current_statistics
+    create_view :episode_current_statistics unless view_exists?(:episode_statistics)
   end
 end
