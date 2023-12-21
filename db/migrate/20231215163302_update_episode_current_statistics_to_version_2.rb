70c51765bcee60bce8077195981f31ea7539e371
michael <m.deimel@miceportal.com>
2023-12-21 21:21:58 +0100

    rubocop

--- db/migrate/20231215163302_update_episode_current_statistics_to_version_2.rb
+++ db/migrate/20231215163302_update_episode_current_statistics_to_version_2.rb
@@ -1,6 +1,5 @@
 class UpdateEpisodeCurrentStatisticsToVersion2 < ActiveRecord::Migration[7.1]
   def change
-  
     update_view :episode_current_statistics, version: 2, revert_to_version: 1
   end
 end
