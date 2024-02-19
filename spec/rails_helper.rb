23f03a3831aef666aa31e931d9625ffaa387bed6
michael <m.deimel@miceportal.com>
2024-02-19 11:06:20 +0100

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,8 +44,9 @@ module JobHelpers
 end
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = Rails.root.join("spec/fixtures").to_s
+  config.fixture_paths = [
+    Rails.root.join("spec/fixtures")
+  ]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
