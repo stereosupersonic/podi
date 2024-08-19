6aede8a904d7605916842a8f8d8460c198c5863d
MICE Portal <michael@deimel.de>
2024-08-19 16:19:06 +0200

    Update statistics_spec.rb

--- spec/system/admin/statistics_spec.rb
+++ spec/system/admin/statistics_spec.rb
@@ -15,50 +15,52 @@ describe "Statistics", type: :system do
       create(:event, episode: episode, created_at: 1.day.ago)
       create(:event, episode: episode, created_at: 3.days.ago)
       create(:event, episode: episode, created_at: 7.days.ago)
+   
+      travel_to Time.current.beginning_of_day do
+        visit "/"
+        click_on "Statistics"
 
-      visit "/"
-      click_on "Statistics"
+        within "#current_statistics" do
+          expect(page).to have_selector "h2", text: "Current Statistics"
+          expect(page).to have_table_with_exact_data([ [
+                                                       "Titel",
+                                                       "Published",
+                                                       "last 12h",
+                                                       "last 24h",
+                                                       "last 3 days",
+                                                       "last 1 week",
+                                                       "last 30 days",
+                                                       "last 60 days",
+                                                       "last 12 month",
+                                                       "last 24 month"
+                                                     ],
+                                                       [ "Soli Wartenberg",
+                                                         "8 days",
+                                                         "1",
+                                                         "1",
+                                                         "2",
+                                                         "3",
+                                                         "-",
+                                                         "-",
+                                                         "-",
+                                                         "-" ] ])
+        end
 
-      within "#current_statistics" do
-        expect(page).to have_selector "h2", text: "Current Statistics"
-        expect(page).to have_table_with_exact_data([ [
-                                                     "Titel",
-                                                     "Published",
-                                                     "last 12h",
-                                                     "last 24h",
-                                                     "last 3 days",
-                                                     "last 1 week",
-                                                     "last 30 days",
-                                                     "last 60 days",
-                                                     "last 12 month",
-                                                     "last 24 month"
-                                                   ],
-                                                     [ "Soli Wartenberg",
-                                                       "9 days",
-                                                       "1",
-                                                       "1",
-                                                       "2",
-                                                       "3",
-                                                       "-",
-                                                       "-",
-                                                       "-",
-                                                       "-" ] ])
-      end
+        none_visible_episode = create(:episode, published_on: 3.weeks.ago, title: "not shown", number: 1)
+        create(:event, episode: none_visible_episode, created_at: 2.weeks.ago)
 
-      none_visible_episode = create(:episode, published_on: 3.weeks.ago, title: "not shown", number: 1)
-      create(:event, episode: none_visible_episode, created_at: 2.weeks.ago)
+        visit "/"
+        click_on "Statistics"
 
-      visit "/"
-      click_on "Statistics"
-
-      within "#overall_statistics" do
-        expect(page).to have_selector "h2", text: "Overall Statistics"
-        expect(page).to have_table_with_exact_data([
-                                                     [ "Titel", "Published", "After 12h", "After 1 day", "After 3 days", "After 1 week", "After 30 days",
-                                                       "After 60 days", "After 12 month", "After 24 month", "overall" ],
-                                                     [ "Soli Wartenberg", "9 days", "-", "-", "1", "2", "3", "3", "3",
-                                                       "3", "3" ]
-                                                   ])
+        within "#overall_statistics" do
+          expect(page).to have_selector "h2", text: "Overall Statistics"
+          expect(page).to have_table_with_exact_data([
+                                                       [ "Titel", "Published", "After 12h", "After 1 day", "After 3 days", "After 1 week", "After 30 days",
+                                                         "After 60 days", "After 12 month", "After 24 month", "overall" ],
+                                                       [ "Soli Wartenberg", "8 days", "-", "-", "1", "2", "3", "3", "3",
+                                                         "3", "3" ]
+                                                     ])
+        end
       end
     end
   end
