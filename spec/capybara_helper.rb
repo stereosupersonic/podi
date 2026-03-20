91e516c1c51b52cc5aa4d56b67ce49e2114ff3fb
MICE Portal <michael@deimel.de>
2026-03-20 10:04:48 +0100

    Update capybara_helper.rb

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -26,6 +26,7 @@ RSpec.configure do |config|
       options.add_argument("--no-sandbox")
       options.add_argument("--no-default-browser-check")
       options.add_argument("--disable-gpu")
+      options.add_argument("--disable-features=HttpsUpgrades")
 
       Capybara::Selenium::Driver.new(app,
                                      browser: :remote,
